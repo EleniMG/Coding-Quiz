c7efb5e10f6e608300d0aa986b0aef2f58d7d2de
Eleni <elenimariex@gmail.com>
2023-11-05 21:08:21 +0000

    Retrieving playerDetails from local storage, pushing initials and score when submit button is clicked

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -31,8 +31,9 @@ function gameOver() {
   feedbackSection.setAttribute("class", "hide");
   endScreen.setAttribute("class", "none");
 }
-
-var allPlayerDetails = [];
+// Need to retrieve the array of player details from storage so that it is not overwritten with an empty array
+var playerDetailsFromStorage = localStorage.getItem("allPlayerDetails");
+var allPlayerDetails = JSON.parse(playerDetailsFromStorage);
 var initialsEntered = document.querySelector("#initials");
 var initialsSubmitButton = document
   .querySelector("#submit")
