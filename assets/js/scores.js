532bd0530353be88785743d5632a6d79cb836337
Eleni <elenimariex@gmail.com>
2023-11-05 21:27:31 +0000

    Creating a displayHighscores function to show all scores on screen from local storage

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,6 +1,20 @@
 // Use local storage to retrieve the initials and score
-var playerDetails = window.localStorage.getItem("playerDetails");
-var playerDetailsObject = JSON.parse(playerDetails);
+var allPlayerDetails = window.localStorage.getItem("allPlayerDetails");
+var playerDetailsArray = JSON.parse(allPlayerDetails);
+console.log(playerDetailsArray, playerDetailsArray.length);
 
 // Show these details on screen as a new list item
-var allPlayerScores = [];
+
+var highscoreBoard = document.querySelector("#highscores");
+
+function displayHighscores() {
+  for (var i = 0; i < playerDetailsArray.length; i++) {
+    var player = playerDetailsArray[i];
+
+    var li = document.createElement("li");
+    li.textContent = player.initials + ", " + player.score;
+    highscoreBoard.appendChild(li);
+  }
+}
+
+displayHighscores();
